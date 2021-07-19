9fe3125de1672b614dbf97e9d87f34bc0591b2d9
ShIKULJAK <75026773+ShIKULJAK@users.noreply.github.com>
2021-07-20 01:26:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,289 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" referrerpolicy="no-referrer" />
+
+    <!-- SCSS -->
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Swimming</title>
+</head>
+
+<body>
+    <!-- NAV -->
+
+    <nav class="navbar navbar-expand-lg" id="nav">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Swimming<sub>&reg;</sub></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about-us">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#gallery">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#blog">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contacts">Contacts</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- HEADER -->
+    <header id="header">
+        <!-- img -->
+    </header>
+
+
+    <!-- CARDS -->
+
+    <section class="cards" id="about-us">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-sm-12 pt-5">
+                    <div class="card-1">
+                        <div class="card-body">
+                            <h5 class="card-title display-4">Registar <br> for swim <br> lessons</h5>
+                            <article class="d-flex justify-content-between align-items-end">
+                                <p class="card-text">Our experienced in- <br> structors offer a va- <br> riety of swimming <br> lessons for children br and teens.</p>
+                                <a href="#"><i class="fas fa-chevron-right display-4"></i></a>
+                            </article>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-12 pt-5">
+                    <div class="card-2">
+                        <div class="card-body">
+                            <h5 class="card-title display-4">Registar <br> for swim <br> lessons</h5>
+                            <article class="d-flex justify-content-between align-items-end">
+                                <p class="card-text">Our experienced in- <br> structors offer a va- <br> riety of swimming <br> lessons for children br and teens.</p>
+                                <a href="#"><i class="fas fa-chevron-right display-4"></i></a>
+                            </article>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-12 pt-5">
+                    <div class="card-3">
+                        <div class="card-body">
+                            <h5 class="card-title display-4">Registar <br> for swim <br> lessons</h5>
+                            <article class="d-flex justify-content-between align-items-end">
+                                <p class="card-text">Our experienced in- <br> structors offer a va- <br> riety of swimming <br> lessons for children br and teens.</p>
+                                <a href="#"><i class="fas fa-chevron-right display-4"></i></a>
+                            </article>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-12 pt-5">
+                    <div class="card-4">
+                        <div class="card-body">
+                            <h5 class="card-title display-4">Registar <br> for swim <br> lessons</h5>
+                            <article class="d-flex justify-content-between align-items-end">
+                                <p class="card-text">Our experienced in- <br> structors offer a va- <br> riety of swimming <br> lessons for children br and teens.</p>
+                                <a href="#"><i class="fas fa-chevron-right display-4"></i></a>
+                            </article>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- GALLERY -->
+    <section id="gallery">
+        <div class="container-fluid">
+            <div class="row pb-5">
+                <div class="col-md-6 p-5 d-flex flex-column justify-content-evenly align-items-start">
+                    <h5 class="display-4">Welcome to our school!</h5>
+                    <p>Swimming School is proud to provide state-of-the-art aquatic facilities that are dedicated to indoor swim classes and programs for children.</p>
+                    <button>Read More</button>
+                </div>
+                <div class="col-md-6  g-0">
+                    <img src="img/about1.jpg" alt="">
+                </div>
+            </div>
+            <div class="row pb-5">
+                <div class="col-md-6 col-sm-12 g-0">
+                    <img src="img/about2.jpg" alt="">
+                </div>
+                <div class="col-md-6 col-sm-12 p-5 d-flex flex-column justify-content-evenly align-items-start">
+                    <h5 class="display-4">Swim lessons for Children and teens</h5>
+                    <p>We offer a variety of private and group lessons for children and teens. The lessons and swimming courses include everything your kid needs to swim confidently.
+
+                    </p>
+                    <button>Read More</button>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- BLOG -->
+
+    <section id="blog">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 p-5 d-flex flex-wrap flex-column justify-content-evenly align-items-start">
+                    <h5 class="display-4">Why Our Swimming School is Rated #1 in The Region
+                    </h5>
+                    <p>We offer a variety of private and group lessons for children and teens. The lessons and swimming courses include everything your kid needs to swim confidently. Swimming School is proud to provide state-of-the-art aquatic facilities
+                        that are dedicated to indoor swim classes and programs for children.
+                    </p>
+                    <button>Read More</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- EVENTS -->
+    <section id="events">
+        <div class="container-fluid">
+            <div class="row my-5">
+                <div class="col-md-6 p-5">
+
+                    <h5 class="display-4">Our Events</h5>
+                    <ul class="p-0 d-flex flex-column justify-content-between">
+                        <li><i class="fas fa-chevron-circle-right"></i> Lorem ipsum dolor sit amet consectetur.</li>
+                        <li><i class="fas fa-chevron-circle-right"></i> Lorem ipsum dolor sit amet consectetur.</li>
+                        <li><i class="fas fa-chevron-circle-right"></i> Lorem ipsum dolor sit amet consectetur.</li>
+                        <li><i class="fas fa-chevron-circle-right"></i> Lorem ipsum dolor sit amet consectetur.</li>
+                        <li><i class="fas fa-chevron-circle-right"></i> Lorem ipsum dolor sit amet consectetur.</li>
+                    </ul>
+                </div>
+                <div class="col-md-6 px-0">
+                    <img src="img/events.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- PEOPLE -->
+
+    <section id="people-card">
+        <div class="container-fluid">
+            <div class="row justify-content-between mb-5">
+                <div class="col-lg-3 col-sm-12 p-4">
+                    <img src="img/people1.jpg" alt="">
+
+                    <div>
+                        <p class="text-start my-5">Thank you for making me a confident swimmer! I hope to learn more from the school’s instructors during future.</p>
+                        <h6 class="text-start">Anne, 25 y.o.</h6>
+                    </div>
+
+                </div>
+                <div class="col-lg-3 col-sm-12 p-4">
+                    <img src="img/people2.jpg" alt="">
+
+                    <div>
+                        <p class="text-start my-5">I hated swimming until I joined this swimming school. Their lessons improved my swimming and breathing skills.</p>
+                        <h6 class="text-start">Sam, 23 y.o.</h6>
+                    </div>
+
+                </div>
+                <div class="col-lg-3 col-sm-12 p-4">
+                    <img src="img/people3.jpg" alt="">
+
+                    <div>
+                        <p class="text-start my-5">I have been going here for swimming lessons for over 3 years and it helped me become a better swimmer.</p>
+                        <h6 class="text-start">Peter, 28 y.o.</h6>
+                    </div>
+
+                </div>
+                <div class="col-lg-3 col-sm-12 p-4">
+                    <img src="img/people4.jpg" alt="">
+
+                    <div>
+                        <p class="text-start my-5">Joining this school has changed my life as I was afraid of water before but now I like it a lot thanks to your lessons.</p>
+                        <h6 class="text-start">Jane, 20 y.o.</h6>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- CONTACTS -->
+
+    <section id="contacts">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <h4>Contacts</h4>
+                    <p>Address: 4578 Marmora Road, Glasgow D04 89GR</p>
+                    <span><i class="fas fa-mobile-alt"></i>&nbsp; 1-800-2345-6789</span>
+                    <p class="my-4">Email:youremil.com</p>
+                    <ul class="d-flex justify-content-start p-0">
+                        <li>
+                            <a href=""><i class="fab fa-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href=""><i class="fab fa-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href=""><i class="fab fa-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href=""><i class="fab fa-whatsapp"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-6 p-0">
+                    <img src="img/img10.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- FOOTER -->
+
+    <footer>
+        <div class="row">
+            <h3 class="h6 text-center mx-auto pb-5">Swimming &copy; 2021. All Rights Reserved. Privacy Policy.</h3>
+        </div>
+    </footer>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
